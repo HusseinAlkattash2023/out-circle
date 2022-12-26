4e1424cd7ef0713df6f3d4ead65015cf1e423833
hussein <asd>
2022-12-26 17:21:56 +0200

    Updates

--- static/js/5.2dafb3c1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[5],{1239:function(e,a,c){c.r(a);c(72791);var n=c(20839),r=c(43005),t=c(80184);a.default=function(){return(0,t.jsx)("div",{children:(0,t.jsx)(r.Z,{image:n})})}}}]);
-//# sourceMappingURL=5.2dafb3c1.chunk.js.map
