ff57095488013fc843d0b71ed8e1ed163b6c028a
hussein <asd>
2023-01-04 12:33:38 +0200

    Updates

--- static/js/4651.c058c38c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[4651],{14651:function(e,t,l){l.d(t,{S1K:function(){return r}});var n=l(89983);function r(e){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"}}]})(e)}}}]);
-//# sourceMappingURL=4651.c058c38c.chunk.js.map
